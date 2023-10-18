2da377979ffb6261aefae9474a5315a81f3a67e0
Panaria <baranyldzby@gmail.com>
2023-10-18 21:56:15 +0300

    Add project files.

---
+++ datetimepicker kullanimi/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datetimepicker_kullanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime baslangic, bitis;
+
+            baslangic = dateTimePicker1.Value;
+            bitis = dateTimePicker2.Value;
+
+            TimeSpan ts;
+
+            ts = bitis - baslangic;
+
+            MessageBox.Show(ts.Days.ToString() + " Gün");
+
+        }
+    }
+}
